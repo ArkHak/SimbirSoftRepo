8f26e1bc2d930204abac8fb6c2be0c1a7a0d6725
Oleg Mysin <7nacu7@gmail.com>
2023-06-11 21:59:57 +0300

    Практическое задание по Блоку VI. Верстка: - SplashScreen - done;

---
+++ app/src/main/java/o/mysin/simbirsoftappjava/SplashScreen.kt
@@ -0,0 +1,23 @@
+package o.mysin.simbirsoftappjava
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, SPLASH_SCREEN_TIMEOUT)
+    }
+
+    companion object {
+        private const val SPLASH_SCREEN_TIMEOUT = 3000L
+    }
+}
