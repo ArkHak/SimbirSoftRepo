bc5e590706bacfafec30a0ee740548c188ffceab
Oleg Mysin <7nacu7@gmail.com>
2023-07-09 23:36:09 +0300

    Practical task for Block IX. Multithreading (1) - added data loading via Executors in HelpFragment;

--- app/src/main/java/o/mysin/simbirsoftappjava/ui/help/HelpViewModel.kt
+++ app/src/main/java/o/mysin/simbirsoftappjava/ui/help/HelpViewModel.kt
@@ -7,23 +7,43 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import o.mysin.simbirsoftappjava.domain.repository.HelpCategoryRepository
 import o.mysin.simbirsoftappjava.domain.model.HelpCategory
+import java.util.concurrent.Executors
 
 class HelpViewModel(
     private val helpCategoryRepository: HelpCategoryRepository,
 ) : ViewModel() {
 
+    private val executor = Executors.newSingleThreadExecutor()
+
     private val _helpCategoryList: MutableLiveData<List<HelpCategory>> = MutableLiveData()
     val helpCategoryList: LiveData<List<HelpCategory>>
         get() = _helpCategoryList
 
-    init {
-        loadHelpCategory()
+    override fun onCleared() {
+        super.onCleared()
+        executor.shutdown()
     }
 
-    private fun loadHelpCategory() {
+    fun updateData() {
+        loadHelpCategoryFromExecutor()
+    }
+
+    private fun loadHelpCategoryFromCoroutines() {
         viewModelScope.launch {
             _helpCategoryList.value = helpCategoryRepository.getHelpCategories()
         }
+    }
+
+    private fun loadHelpCategoryFromExecutor() {
+        executor.execute() {
+            Thread.sleep(TIMEOUT)
+            val list = helpCategoryRepository.getHelpCategories()
+            _helpCategoryList.postValue(list)
+        }
+    }
+
+    companion object {
+        private const val TIMEOUT = 5000L
     }
 
 }
