30b06158b7897d011d1b6fa2354768850e4ebd2e
Oleg Mysin <7nacu7@gmail.com>
2023-08-01 21:45:34 +0300

    Practical task for Block X Practice. RxJava(refactoring - 5) - refactoring AssetManagerImpl;

--- app/src/main/java/o/mysin/simbirsoftappjava/utils/AssetManagerImpl.kt
+++ app/src/main/java/o/mysin/simbirsoftappjava/utils/AssetManagerImpl.kt
@@ -12,21 +12,18 @@ class AssetManagerImpl(
     private val context: Context,
 ) : AssetManager {
     override fun getNewsListFromAsset(fileName: String): List<News> {
-        val inputStream = context.assets.open(fileName)
-        val reader = InputStreamReader(inputStream)
-        val listNews = gson.fromJson(reader, Array<News>::class.java).toList()
-        reader.close()
-        inputStream.close()
-        return listNews
+        return context.assets.open(fileName).use { inputStream ->
+            InputStreamReader(inputStream).use { reader ->
+                gson.fromJson(reader, Array<News>::class.java).toList()
+            }
+        }
     }
 
     override fun getHelpCategoryListFromAsset(fileName: String): List<HelpCategory> {
-        val inputStream = context.assets.open(fileName)
-        val reader = InputStreamReader(inputStream)
-        val helpCategory = gson.fromJson(reader, Array<HelpCategory>::class.java).toList()
-        reader.close()
-        inputStream.close()
-        return helpCategory
+        return context.assets.open(fileName).use { inputStream ->
+            InputStreamReader(inputStream).use { reader ->
+                gson.fromJson(reader, Array<HelpCategory>::class.java).toList()
+            }
+        }
     }
 }
-
